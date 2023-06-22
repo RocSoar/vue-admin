ca608178d65eafbc240a5fc70c5e4f8d8f516167
RocSoar <lsyyd3454@outlook.com>
2023-06-22 22:26:56 +0800

    1 - init

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+const str = "Hello, World!"
+</script>
+
+<template>
+  <div class="box">
+    <h1>APP根组件</h1>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.box {
+  width: 600px;
+  height: 400px;
+  background: black;
+}
+
+h1 {
+  color: white;
+}
+</style>
